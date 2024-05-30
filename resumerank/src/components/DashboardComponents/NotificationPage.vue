71085998c37d3ef3444b98aa82ddab1e74b4be3b
chino-m <qlabsph@gmail.com>
2024-05-30 17:52:55 +0800

    Minor Update
    Prepared Notifications Page

--- resumerank/src/components/DashboardComponents/NotificationPage.vue
+++ resumerank/src/components/DashboardComponents/NotificationPage.vue
@@ -1,13 +1,17 @@
 <template>
     <div id="container">
         <h1>Notifications</h1>
-        <p>Alerts at a Glance: Your Notification Nexus.</p>
 
         <div id="notifications">
             <h2>Recent Notifications</h2>
 
-            <div id="all-notifs">
-                <h3>No Notifications.</h3>
+            <h3 v-if="all_notifs.length == 0">No Notifications.</h3>
+
+            <div id="all-notifs" v-else>
+                <div v-for="notif in all_notifs" :key="notif" class="notif">
+                    <h1></h1>
+                    <p></p>
+                </div>
             </div>
         </div>
     </div>
@@ -17,7 +21,9 @@
 export default {
     name: 'NotificationPage',
     data (){
-
+        return {
+            all_notifs: []
+        }
     }
 }
 </script>
