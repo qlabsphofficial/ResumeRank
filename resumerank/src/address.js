f937c07632ba094c9528876124986815a8a344b7
qlabsphofficial <qlabsph@gmail.com>
2024-07-06 13:48:56 +0800

    change address

--- resumerank/src/address.js
+++ resumerank/src/address.js
@@ -1,6 +1,6 @@
-const local_address = 'http://127.0.0.1:8000';
-// const host_address = 'https://resumerank.onrender.com';
+// const local_address = 'http://127.0.0.1:8000';
+const host_address = 'https://resumerank.onrender.com';
 
-const current_address = local_address;
+const current_address = host_address;
 
 export default current_address;
